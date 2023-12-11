1e1ee4b1ba47a0796fbee7fd0074413367868ca5
Luis Fernando <fluisf00@gmail.com>
2023-12-11 14:25:35 -0300

    local changes

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-/* import * as React from 'react'; */
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
